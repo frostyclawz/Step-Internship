64f217321bf62d15964d0f964ef9a5b04c27cb84
Vincent Stevenson <vincentjr34@yahoo.com>
2020-06-04 17:47:58 +0000

    Adding function to parse json data from the servlet and print it to the webpage.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,19 +14,38 @@
 
 package com.google.sps.servlets;
 
+import com.google.gson.Gson;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
+  private String convertToJson(ArrayList<String> comments) {
+    Gson gson = new Gson();
+    String json = gson.toJson(comments);
+    return json;
+  }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("Hello Vincent!");
+    
+    ArrayList<String> messages = new ArrayList<String>();
+    messages.add("Hello");
+    messages.add("This is a message.");
+    messages.add("I am a machine...");
+    
+    String json = convertToJson(messages);
+    
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
   }
 }
+
+
+
+
